468ba90b0dd186bdc46d1ef5848d1dac49fa8e26
Rizwan Ullah <rizwanullahtiger313@gmail.com>
2021-03-12 01:35:49 +0500

    Simple React app passing data through props

---
+++ src/information.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './inf.css';
+
+function information(props)
+{
+return(<div>
+    <center>
+    <p>I like <b> {props.coding}</b>  and <b> {props.codeing} </b> </p>
+    </center>
+</div>)
+}
+
+export default information;
